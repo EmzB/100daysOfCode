28bb91d20752fad07c0071d63ccfb5d7bfe693d5
Bella <emalibella45@gmail.com>
2023-01-25 15:41:45 +0300

    Playing with the DOM

--- day-53/piano.css/script.js
+++ day-53/piano.css/script.js
@@ -1,9 +1,17 @@
-window.addEventListener("load", () => {
-var pianoKeys= document.querySelector('.keys') 
-pianoKeys.forEach(addEventListener('click', (e)=>{
-const audio= document.querySelector(`audio[data-key="${e.target.dataset.key}"]`)
-audio.play()
-}))
+window.addEventListener("load", () => {//init JS after all content loads on the DOM
+console.log("ready when you are!")
+let pianoKeys= document.querySelector('.keys') // selecting all piano keys
+pianoKeys.forEach(
+    addEventListener('click', (e) =>{// looping through each key and adding EventListener
+        
+        /* checking for the audio that matches the data-key of selected key*/
+    const audio= document.querySelector (`audio[data-key="${e.target.dataset.key}"]`)
+
+    audio.play();
+    audio.currentTime=0; // audio playback on multiple clicks
+})
+
+)
 
 
 
