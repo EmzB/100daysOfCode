54b49b749220b73ae21800c38f0bea821b86cc3e
Bella <emalibella45@gmail.com>
2023-01-25 17:02:45 +0300

    adding play with keyboard functionality

--- day-53/piano.css/script.js
+++ day-53/piano.css/script.js
@@ -1,18 +1,20 @@
-window.addEventListener("load", () => {//init JS after all content loads on the DOM
+window.addEventListener('load', () => { //init JS after all content loads on the DOM
 console.log("ready when you are!")
+
 let pianoKeys= document.querySelector('.keys') // selecting all piano keys
-pianoKeys.forEach(
-    addEventListener('click', (e) =>{// looping through each key and adding EventListener
-        
-        /* checking for the audio that matches the data-key of selected key*/
-    const audio= document.querySelector (`audio[data-key="${e.target.dataset.key}"]`)
+pianoKeys.forEach( addEventListener('click', (e)=> {
+let audio= document.querySelector (`audio[data-key="${e.target.dataset.key}"]`)
 
-    audio.play();
-    audio.currentTime=0; // audio playback on multiple clicks
+audio.play()
+audio.currentTime=0; // audio playback on multiple clicks
+
+}) )})
+
+window.addEventListener('keydown', (e) => {
+//first check if pressed key matches a corresponding data-key if! return
+let audio= document.querySelector(`audio[data-key="${e.keyCode}"]`) 
+if (!audio) return; 
+audio.currentTime=0; 
+audio.play()
+    
 })
-
-)
-
-
-
-});
