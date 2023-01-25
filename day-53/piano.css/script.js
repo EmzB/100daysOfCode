e146f9ceb0d6ae7099ded256efaf51ed778e4dc7
Bella <emalibella45@gmail.com>
2023-01-25 19:18:41 +0300

    handling volume

--- day-53/piano.css/script.js
+++ day-53/piano.css/script.js
@@ -7,6 +7,7 @@ let audio= document.querySelector (`audio[data-key="${e.target.dataset.key}"]`)
 
 audio.play()
 audio.currentTime=0; // audio playback on multiple clicks
+//audio.volume= volumeMixer()
 
 }) )})
 
@@ -16,5 +17,16 @@ let audio= document.querySelector(`audio[data-key="${e.keyCode}"]`)
 if (!audio) return; 
 audio.currentTime=0; 
 audio.play()
+//audio.volume= volumeMixer()
     
 })
+
+const volumeRange = document.querySelector('.range input')
+volumeRange.addEventListener('input', volumeMixer)
+
+var volumeMixer = function (input){
+    console.log (input.target.value)
+    //return input.target.value
+
+}
+
