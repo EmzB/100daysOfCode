b5ba6d40ba5793413fd2a781c424edd4901a1a4f
Bella <emalibella45@gmail.com>
2023-01-31 23:24:32 +0300

    date works!

--- day-56/Frontend/clock.js/index.js
+++ day-56/Frontend/clock.js/index.js
@@ -2,5 +2,12 @@ const secHand= document.querySelector('.sec-hand')
 const minHand= document.querySelector('.min-hand')
 const hrHand= document.querySelector('.hr-hand')
 
-getDate =()=> (Date())
-console.log(getDate())
+var date = new Date()
+console.log(date)
+
+seconds= date.getSeconds();
+console.log (seconds)
+minutes= date.getMinutes();
+console.log (minutes)
+hours=  date.getHours();
+console.log (hours)
