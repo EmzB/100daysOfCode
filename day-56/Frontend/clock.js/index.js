f26d69a5cbf6e538c1a452963c46b78572e315ac
Bella <emalibella45@gmail.com>
2023-01-31 23:11:25 +0300

    DOM manipulation

---
+++ day-56/Frontend/clock.js/index.js
@@ -0,0 +1,6 @@
+const secHand= document.querySelector('.sec-hand')
+const minHand= document.querySelector('.min-hand')
+const hrHand= document.querySelector('.hr-hand')
+
+getDate =()=> (Date())
+console.log(getDate())
