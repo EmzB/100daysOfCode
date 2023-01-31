c4dc66c7cd074031f27f6ca4dbaa2319d0181b71
Bella <emalibella45@gmail.com>
2023-02-01 00:05:23 +0300

    getting errors using setProperty finish tomorrow

--- day-56/Frontend/clock.js/index.js
+++ day-56/Frontend/clock.js/index.js
@@ -1,13 +1,22 @@
 const secHand= document.querySelector('.sec-hand')
 const minHand= document.querySelector('.min-hand')
 const hrHand= document.querySelector('.hr-hand')
+//setInterval(moveClock, 1000)
 
+function moveClock(){
 var date = new Date()
 console.log(date)
 
-seconds= date.getSeconds();
+seconds= date.getSeconds()/60;
 console.log (seconds)
-minutes= date.getMinutes();
+minutes= (seconds+ date.getMinutes())/60;
 console.log (minutes)
-hours=  date.getHours();
+hours=  (minutes+ date.getHours())/12;
 console.log (hours)
+
+
+secHand.style.setProperty('--rotationValue', seconds*360)
+minHand.style.setProperty('--rotationValue', minutes*360 )
+hrHand.style.setProperty('--rotationValue', hours*360 )
+}
+moveClock();
