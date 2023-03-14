99aeb78ac705fc531e656c0d6b296c03a00d7bfb
Bella <emalibella45@gmail.com>
2023-03-14 11:34:50 +0300

    board generated

--- day-64/index.js
+++ day-64/index.js
@@ -2,13 +2,17 @@ const squaresContainer= document.querySelector('.squares-container');
 const scores= document.querySelector('.score');
 const squares = ["", "", "", "", "","","","",""]
 
-console.log(scores.className, squaresContainer.className );
+console.log(scores.className, squaresContainer.className);
 
 function formBoard(){
-squares.forEach((square)=>{
+squares.forEach((square, i)=>{
 
 const singleSquare= document.createElement('div');
 console.log(singleSquare);
+singleSquare.id= i;
+singleSquare.classList.add('single-square')
+console.log(singleSquare.className)
+squaresContainer.append(singleSquare)
 
 })
 }
