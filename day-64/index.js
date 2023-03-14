518dad3b721d00bdfe8362b8f84467f86d787f20
Bella <emalibella45@gmail.com>
2023-03-14 11:56:50 +0300

    state works!

--- day-64/index.js
+++ day-64/index.js
@@ -1,6 +1,7 @@
 const squaresContainer= document.querySelector('.squares-container');
 const scores= document.querySelector('.score');
-const squares = ["", "", "", "", "","","","",""]
+const squares = ["", "", "", "", "","","","",""];
+let currentValue= "X";
 
 console.log(scores.className, squaresContainer.className);
 
@@ -10,11 +11,19 @@ squares.forEach((square, i)=>{
 const singleSquare= document.createElement('div');
 console.log(singleSquare);
 singleSquare.id= i;
-singleSquare.classList.add('single-square')
-console.log(singleSquare.className)
-squaresContainer.append(singleSquare)
+singleSquare.classList.add('single-square');
+singleSquare.addEventListener('click', addState)
+console.log(singleSquare.className);
+squaresContainer.append(singleSquare);
 
 })
+}
+
+function addState(e){
+console.log("i've been clicked adding state...");
+e.target.textContent= currentValue;
+
+
 }
 
 formBoard()
