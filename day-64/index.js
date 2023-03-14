b16ead6e6870fd6720069bd81501bdd87d31d684
Bella <emalibella45@gmail.com>
2023-03-14 04:38:01 +0300

    autogenerating squares

---
+++ day-64/index.js
@@ -0,0 +1,16 @@
+const squaresContainer= document.querySelector('.squares-container');
+const scores= document.querySelector('.score');
+const squares = ["", "", "", "", "","","","",""]
+
+console.log(scores.className, squaresContainer.className );
+
+function formBoard(){
+squares.forEach((square)=>{
+
+const singleSquare= document.createElement('div');
+console.log(singleSquare);
+
+})
+}
+
+formBoard()
